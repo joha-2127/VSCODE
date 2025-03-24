92ef2474be4ba1d121e21c4347914c8835d9d8eb
joha-2127 <jarhana05@gmail.com>
2025-03-25 02:56:59 +0600

    changed to Shotgun123

--- array.java
+++ array.java
@@ -1,12 +1,11 @@
 public class array {
-    public static void main(String[]args){
-        String backpack[]={"Shotgun","Snipper","Rifle"};
-        String zombie[]={"mid range zombie","9","front zombie","close range zombie"};
+    public static void main(String[] args) {
+        String backpack[] = { "Shotgun123", "Snipper", "Rifle" };
+        String zombie[] = { "mid range zombie", "9", "front zombie", "close range zombie" };
 
         System.out.println(backpack[0]);
         System.out.println(zombie[1]);
         System.out.println(backpack[2]);
-
 
     }
 }
