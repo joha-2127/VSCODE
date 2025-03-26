e02875a7c459853b694021fcba26a485816a150d
joha-2127 <jarhana05@gmail.com>
2025-03-26 21:01:48 +0600

    initial commit

---
+++ array2d.java
@@ -0,0 +1,8 @@
+public class array2D {
+    public static void main(String[] args) {
+        int[][] twodimensional = { { 1, 2, 3 },
+                { 5, 6, 7 },
+                { 8, 9, 10 } };
+        System.out.println(twodimensional[0][0]);
+    }
+}
