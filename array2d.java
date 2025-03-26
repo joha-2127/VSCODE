96c7418cfad26f719b9662241205ab8ce024ad6c
joha-2127 <jarhana05@gmail.com>
2025-03-26 21:15:22 +0600

    new data

--- array2d.java
+++
@@ -1,8 +0,0 @@
-public class arr {
-    public static void main(String[] args) {
-        int[][] twodimensional = { { 1, 2, 3 },
-                { 5, 6, 7 },
-                { 8, 9, 10 } };
-        System.out.println(twodimensional[0][0]);
-    }
-}
