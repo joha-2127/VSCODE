15f1b7ad0882df4a47ce1f982934c1b00942bc02
joha-2127 <jarhana05@gmail.com>
2025-04-22 09:42:15 +0600

    initial commit

---
+++ Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher {
+    String name,gender;
+    int phone;
+}
+public class Test{
+   public static void main (String []args)
+   Teacher teacher1;
+   teacher1= new Teacher();
+   teacher
+}
